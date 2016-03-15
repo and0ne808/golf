446d50e47542d317f3bb64490cb5add2f426e10b
Andrew Day <and0ne808@gmail.com>
2016-03-15 10:12:49 -0600

    Added some level geometry

--- Rolly Polly/Assets/Scripts/LookAtTarget.cs
+++ Rolly Polly/Assets/Scripts/LookAtTarget.cs
@@ -5,9 +5,12 @@ public class LookAtTarget : MonoBehaviour {
 
     private Transform target;
     public GameObject look;
+    public RectTransform cameraTransform;
+    public Vector3 cameraRotation;
 
 	// Use this for initialization
 	void Start () {
+
         target = look.GetComponent<Transform>();
 	}
 	
@@ -16,8 +19,8 @@ public class LookAtTarget : MonoBehaviour {
 	
         if(target != null)
         {
-            transform.LookAt(target);
-            transform.Rotate(0, 180, 0);
+            transform.LookAt(target,this.transform.up);
+            //transform.Rotate(0, 0, 0);
             transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
 
         }
--- Rolly Polly/Assets/Scripts/cameraController.cs
+++ Rolly Polly/Assets/Scripts/cameraController.cs
@@ -6,14 +6,39 @@ public class cameraController : MonoBehaviour {
   public GameObject player;
 
     private Vector3 offset;
+    public Vector3 zoomVector;
+    public float zoom;
 
     void Start ()
     {
         offset = transform.position - player.transform.position;
+        zoomVector = Vector3.zero;
+    }
+
+    void Update()
+    {
+        if (zoom <= 3 && zoom >= -50)
+        {
+
+            zoom += (Input.GetAxis("RightV"));
+            Debug.Log(zoom);
+            zoomVector = new Vector3(0, -zoom, zoom);
+        }
+
+        if (zoom > 3)
+        {
+            zoom = 3;
+            zoomVector = new Vector3(0, -zoom, zoom);
+        }
+        if (zoom < -50)
+        {
+            zoom = -50;
+            zoomVector = new Vector3(0, -zoom, zoom);
+        }
     }
     
     void LateUpdate ()
     {
-        transform.position = player.transform.position + offset;
+        transform.position = player.transform.position + offset + zoomVector;
     }
 }
